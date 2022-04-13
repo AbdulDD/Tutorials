f010221917db26b5b158f911e6201fdd62a95ed2
Abdul Muqtadir <73823824+AbdulDD@users.noreply.github.com>
2022-04-13 11:32:28 -0700

    Kaggle Notebook | Customized Py Library | Version 30

--- customized-py-library.py
+++ customized-py-library.py
@@ -123,15 +123,19 @@ def load_image_files(container_path, Dimension_value):
                  images=images,
                  DESCR=descr)
 
-def About_Array(Array):
+#def About_Array(Array):
     
-    print('Data type:', Array.dtype)
-    print('Dimension of array:', Array.ndim)
-    print('Itemsize:', Array.itemsize)
-    print('Shape:', Array.shape)
-    print('Memory location:', Array.data)
-    print('Strides:', Array.strides)
-    print('-------------------------------------')
+#    print('Data type:', Array.dtype)
+#    print('Dimension of array:', Array.ndim)
+#    print('Itemsize:', Array.itemsize)
+#    print('Shape:', Array.shape)
+#    print('Memory location:', Array.data)
+#    print('Strides:', Array.strides)
+#    print('-------------------------------------')
+    
+def About_Array(Array):
+    A_Array = pd.DataFrame({'Dimension of array': Array.ndim,'Data type': Array.dtype,'Shape': Array.shape,'Size': Array.size,'Item size': Array.itemsize,'Number of bytes': Array.nbytes,'Memory location': Array.data,'Strides': Array.strides})
+    return A_Array
 
 # %% [markdown]
 # # Visualization commands
